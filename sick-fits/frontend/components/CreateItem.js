6df9d41071b0aaf85bd9a709d977cd612c58e052
Mac <mac@Edit5-2.local>
2018-12-11 15:22:33 -0500

    cloudinary set up

--- sick-fits/frontend/components/CreateItem.js
+++ sick-fits/frontend/components/CreateItem.js
@@ -28,17 +28,35 @@ const CREATE_ITEM_MUTATION = gql `
 
 class CreateItem extends Component {
     state = {
-        title: 'Cool Shoes',
-        description: 'I love those Context',
-        image: 'dog.jpg',
-        largeImage: 'large-dog.jpg',
-        price: 999,
+        title: '',
+        description: '',
+        image: '',
+        largeImage: '',
+        price: '',
     };
     handleChange = (e) => {
         const { name, type, value } = e.target;
         const val = type === 'number' ? parseFloat(value) : value;
         this.setState({ [name]: val })
-    }
+    };
+
+    uploadFile = async (e) => {
+        const files = e.target.files;
+        const data = new FormData()
+        data.append('file', files[0])
+        data.append('upload_preset','sickfits');
+
+        const res = await fetch('https://api.cloudinary.com/v1_1/pcntv/image/upload', {
+            method: 'POST',
+            body: data
+        });
+        const file = await res.json();
+        console.log(file)
+        this.setState({
+            image: file.secure_url,
+            largeImage: file.eager[0].secure_url
+        })
+    };
 
     render() {
         return (
@@ -61,6 +79,19 @@ class CreateItem extends Component {
     >
     <Error error={error}/>
         <fieldset disabled={loading} aria-busy={loading}>
+        <label 
+                htmlFor="file">
+                    Image
+                <input 
+                    type="file" 
+                    id="file" 
+                    name="file" 
+                    placeholder="Upload an image" required 
+                    onChange={this.uploadFile}
+                    />
+                    {this.state.image && <img width="200" src={this.state.image} alt="Upload Preview"/>}
+                </label>
+
             <label 
                 htmlFor="title">
                     Title
