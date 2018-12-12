57795fe756db77381ca74598ec3cc5510eef3043
Mac <mac@Edit1East.local>
2018-12-12 14:36:31 -0500

    delete item

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -15,6 +15,7 @@ const Mutations = {
   
       return item;
     },
+
     updateItem(parent, args, ctx, info) {
       // first take a copy of the updates
       const updates = {...args };
@@ -30,6 +31,18 @@ const Mutations = {
       info
       );
     },
+
+    async deleteItem(parent, args, ctx, info){
+      const where = { id: args.id };
+      //1. find the item
+      const item = await ctx.db.query.item({ where }, `{ id title}`)
+      //2. check if they own the item or have the permissions
+      // TODO
+      //3. Delete it!
+      return ctx.db.mutation.deleteItem( {where }, info)
+    }
+
+
     // createDog(parent, args, ctx, info) {
     //   global.dogs = global.dogs || [];
     //   // create a dog
