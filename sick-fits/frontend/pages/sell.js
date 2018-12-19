f18517a0167e2d49303709eeeb30e216bc68e98f
Mac <mac@Edit5-2.local>
2018-12-19 11:49:36 -0500

    Sign in permission wraper component

--- sick-fits/frontend/pages/sell.js
+++ sick-fits/frontend/pages/sell.js
@@ -1,9 +1,11 @@
-import Link from 'next/link';
 import CreateItem from '../components/CreateItem';
+import PleaseSignIn from '../components/PleaseSignin';
 
 const Sell = props => (
     <div>
+        <PleaseSignIn>
         <CreateItem />
+        </PleaseSignIn>
     </div>
 );
 
