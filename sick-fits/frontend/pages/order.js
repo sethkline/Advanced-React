ad9ba565b9c29b9d69718e6a5416514341f35a9f
Mac <mac@Edit1East.local>
2018-12-21 16:22:49 -0500

    cart credit card

---
+++ sick-fits/frontend/pages/order.js
@@ -0,0 +1,11 @@
+import PleaseSignIn from '../components/PleaseSignin';
+
+const Order = props => (
+    <div>
+        <PleaseSignIn>
+        <p> This is a single order</p>
+        </PleaseSignIn>
+    </div>
+);
+
+export default Order;
