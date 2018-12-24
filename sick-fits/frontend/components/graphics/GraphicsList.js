716ecdb1fd6a4a7057f3c69165c2d1d0cb8f5e24
Mac <mac@Edit5-2.local>
2018-12-24 11:51:38 -0500

    graphics

---
+++ sick-fits/frontend/components/graphics/GraphicsList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Query} from 'react-apollo';
+// import { formatDistance } from 'date-fns';
+import Link from 'next/link';
+import styled from 'styled-components';
+import gql from 'graphql-tag';
+import Error from './ErrorMessage';
+// import formatMoney from '../lib/formatMoney';
+// import OrderItemStyles from './styles/OrderItemStyles';
+
+class GraphicsList extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>Graphics List Component</p>
+            </div>
+        );
+    }
+}
+
+export default GraphicsList;
---
+++ sick-fits/frontend/pages/graphics.js
@@ -0,0 +1,11 @@
+import GraphicsList from '../components/graphics/GraphicsList';
+
+const graphicsPage = props => (
+    <div>
+
+        <GraphicsList />
+
+    </div>
+);
+
+export default graphicsPage;
