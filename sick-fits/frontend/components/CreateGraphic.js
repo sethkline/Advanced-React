abcc2bf4b09d6afdf332cc36776c0fd2d602e860
Mac <mac@Edit5-2.local>
2019-01-17 16:47:45 -0500

    update

--- sick-fits/frontend/components/CreateGraphic.js
+++ sick-fits/frontend/components/CreateGraphic.js
@@ -73,21 +73,6 @@ console.log(nextNumber[0])
 
 
 
-// console.log(misingNumInSeq(misingNumInSeqs))
-
-function misingNumInSeq(source, min = 0, max = source.length - 1){
-    if(min >= max){
-        return min + 1;
-    }
-    let pivot = Math.floor((min + max)/2);
-    // problem is in right side. Only look at right sub array
-    if(source[pivot] === pivot + 1){
-        return misingNumInSeq(source, pivot + 1, max);
-    } else {
-        return misingNumInSeq(source, min , pivot);
-    }
-} 
-
 class CreateGraphic extends Component {
     state = {
         number: '',
